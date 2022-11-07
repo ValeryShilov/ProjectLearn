5e798398994d41f9d497a5ac3f2e0cc5f20ac1c8
ValeryShilov <valera.shilov.03@mail.ru>
2022-11-07 23:29:37 +0500

    Update ProjectLearn.cpp

--- ProjectLearn/ProjectLearn.cpp
+++ ProjectLearn/ProjectLearn.cpp
@@ -4,17 +4,15 @@ using namespace std;
 
 int main()
 {
-    int i;
-    for (i = 0; i <= 100; i++){
-        if (i % 2 == 0) cout << "nechetnoe" << endl;
-    }    
     setlocale(LC_ALL, "Rus");
     int r, R, x, y;
     //x^2+y^2 == R^2
     cout << "Ввeдите R, r, x,y" << endl;
     cin >> R >> r >> x >> y;
-    if ((x*x + y*y < R*R) && (x*x + y*y > r*r))
-        cout << "Попадание" << endl;
+    if (x*x + y*y < R*R)
+        if (x * x + y * y > r * r)
+            cout << "Попадание" << endl;
     else cout << "Промах" << endl;
     return 0;
+
 }
