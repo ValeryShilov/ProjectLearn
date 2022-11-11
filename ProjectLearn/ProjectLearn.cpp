da8b615de04c4e014e755f89de6903e616a3ec48
ValeryShilov <valera.shilov.03@mail.ru>
2022-11-12 00:46:27 +0500

    Update ProjectLearn.cpp
    Поиск минимального, среднего и максимального

--- ProjectLearn/ProjectLearn.cpp
+++ ProjectLearn/ProjectLearn.cpp
@@ -5,14 +5,46 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "Rus");
-    int r, R, x, y;
-    //x^2+y^2 == R^2
-    cout << "Ввeдите R, r, x,y" << endl;
-    cin >> R >> r >> x >> y;
-    if (x*x + y*y < R*R)
-        if (x * x + y * y > r * r)
-            cout << "Попадание" << endl;
-    else cout << "Промах" << endl;
+    int a, b, c, m1, m2, m3;
+    char min_n, max_n, avr_n;
+    cin >> a >> b >> c;
+    if (a > b) {
+        if (c > a) {
+            m3 = a;
+            m1 = b;
+            m2 = c;
+        }
+        else {
+            m2 = a;
+            if (c > b) {
+                m3 = c;
+                m1 = b;
+            }
+            else {
+                m3 = b;
+                m1 = c;
+            }
+        }
+    }
+    else {
+        if (c > b) {
+            m3 = b;
+            m1 = a;
+            m2 = c;
+        }
+        else {
+            m2 = b;
+            if (c > a) {
+                m3 = c;
+                m1 = a;
+            }
+            else {
+                m3 = a;
+                m1 = c;
+            }
+        }
+    }
+    cout << "Минимальное:"<< m1 << " " << "Максимальное:" << m2 << " " << "Cреднее:" << m3 << endl;
     return 0;
 
 }
