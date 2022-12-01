620befc15480fff3e24c38da8ba1802c3adb049e
ValeryShilov <valera.shilov.03@mail.ru>
2022-12-01 15:41:31 +0500

    Update ProjectLearn.cpp

--- ProjectLearn/ProjectLearn.cpp
+++ ProjectLearn/ProjectLearn.cpp
@@ -1,32 +1,28 @@
 ﻿#include <iostream>
-#include <math.h>
+#include <ctime>
 using namespace std;
 
 int main()
 {
     setlocale(LC_ALL, "Rus");
     
-    int a[10]{ 12,2398,8,123,2134,-4,-6,9,23,3 };
-    int mx = -9999999, mn = 9999999;
-    int mx_n, mn_n, cnt = 0;
+    int n, i, j, l;
+    cin >> n;
 
-    int leight = sizeof(a) / sizeof(a[0]);
-    
-    for (int i = 0; i < leight; i++) {
-        if (a[i] > mx) {
-            mx = a[i];
-            mx_n = i;
-        }
-        if (a[i] < mn) {
-            mn = a[i];
-            mn_n = i;
-        }
+    for (i = 1; i <= n; i++) {
+        cout << "* ";
     }
-    for (int j = mx_n+1; j < mn_n; j++) {
-        if (a[j] > 0) cnt++;
+    cout << endl;
+    for (j = 1; j <= n - 2; j++) {
+        cout << "*";
+        for (l = 1; l <= (n - 2)*2 +1; l++) {
+            cout << " ";
+        }
+        cout << "*" << endl;
     }
-    cout << cnt << endl;
-
+    for (int k = 1; k <= n; k++) {
+        cout << "* ";
+    }
     return 0;
 
 }
