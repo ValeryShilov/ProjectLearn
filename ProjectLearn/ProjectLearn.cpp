63aec8779290e9301b70feb8dfed930a5e27a09f
ValeryShilov <valera.shilov.03@mail.ru>
2022-12-13 00:19:16 +0500

    Update ProjectLearn.cpp

--- ProjectLearn/ProjectLearn.cpp
+++ ProjectLearn/ProjectLearn.cpp
@@ -6,23 +6,26 @@ int main()
 {
     setlocale(LC_ALL, "Rus");
     
-    int n, i, j, l, k;
+    int n, i, j, l, k, sp;
     cin >> n;
     
     if (n % 2 != 0) {
-        for (k = 1; k <= n / 2 + 1; k++) {
-            for (i = 1; i <= (n-k) / 2; i++) {
+        sp = n / 2;
+        for (i = 1; i <= n / 2 + 1; i++) {
+            for (j = sp; j >= 0; j--) {
                 cout << " ";
             }
-            for (j = 1; j <= k*2-1; j++) {
+            for (k = 1; k <= n - sp * 2; k++) {
                 cout << "*";
             }
-            for (l = 1; l <= (n-k) / 2; l++) {
+            for (j = sp; j >= 0; j--) {
                 cout << " ";
             }
             cout << endl;
+            sp--;
         }
     }
+    else cout << "Треугольник построить нельзя" << endl;
     return 0;
 
 }
